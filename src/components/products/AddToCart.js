ac5f2b1a5e0fa5b5bb9b639fe0b94b92e0146dd1
sabbir hossain <sabbir.s.dk7@gmail.com>
2021-08-13 21:47:15 +0600

    Add to ammount in card

--- src/components/products/AddToCart.js
+++ src/components/products/AddToCart.js
@@ -3,11 +3,31 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { FaCheck } from "react-icons/fa";
 
+import AmountButtons from "./AmountButtons";
 const AddToCart = ({ product }) => {
   const { id, stock, colors } = product;
 
   const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
 
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    });
+  };
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    });
+  };
   return (
     <Wrapper>
       <div className="colors">
@@ -28,6 +48,20 @@ const AddToCart = ({ product }) => {
             );
           })}
         </div>
+      </div>
+      <div className="btn-container">
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+        <Link
+          to="/cart"
+          className="btn"
+          onClick={() => AddToCart(id, mainColor, amount, product)}
+        >
+          add to cart
+        </Link>
       </div>
     </Wrapper>
   );
