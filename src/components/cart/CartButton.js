6411f25a61e1aea606fe7134726f054e90fa39ce
sabbir hossain <sabbir.s.dk7@gmail.com>
2021-08-22 19:21:48 +0600

    totall amounts

--- src/components/cart/CartButton.js
+++ src/components/cart/CartButton.js
@@ -3,15 +3,17 @@ import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { useProductsContext } from "../../context/products/products_context";
+import { useCartContext } from "../../context/cart/cart_context";
 const CartButton = () => {
   const { closeSidebar } = useProductsContext();
+  const { total_items, clearCart } = useCartContext();
   return (
     <Wrapper className="cart-btn-wrapper">
       <Link to="/cart" className="cart-btn" onClick={closeSidebar}>
         Cart
         <span className="cart-container">
           <FaShoppingCart />
-          <span className="cart-value">12</span>
+          <span className="cart-value">{total_items}</span>
         </span>
       </Link>
       <button className="auth-btn">
