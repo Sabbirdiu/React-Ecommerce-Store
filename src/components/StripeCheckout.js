f5fa3164d18eb731e5367959d549cd77f31db253
sabbir hossain <sabbir.s.dk7@gmail.com>
2021-08-30 22:46:56 +0600

    Added stripe checkout import

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  CardElement,
+  useStripe,
+  Elements,
+  useElements,
+} from "@stripe/react-stripe-js";
+import axios from "axios";
+import { useCartContext } from "../context/cart/cart_context";
+import { useUserContext } from "../context/user/user_context";
+import { formatPrice } from "../utils/helpers";
+import { useHistory } from "react-router-dom";
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const CheckoutForm = () => {
   return <h2>Hello from stripe checkout form</h2>;
 };
 const StripeCheckout = () => {
   return (
-    <Wrapper>
+    <Elements stripe={promise}>
       <CheckoutForm />
-    </Wrapper>
+    </Elements>
   );
 };
 const Wrapper = styled.section`
